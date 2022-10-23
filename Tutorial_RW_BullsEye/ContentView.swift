95974cc50e7d96be6997c81491df55ed58b4ca72
Sedat Onat <sedatonat@outlook.com>
2022-10-23 20:30:19 +0300

    Initial Commit

---
+++ Tutorial_RW_BullsEye/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tutorial_RW_BullsEye
+//
+//  Created by Sedat Onat on 23.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tutorial_RW_BullsEye/Tutorial_RW_BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorial_RW_BullsEyeApp.swift
+//  Tutorial_RW_BullsEye
+//
+//  Created by Sedat Onat on 23.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial_RW_BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
