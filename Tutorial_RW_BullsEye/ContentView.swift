7eee65783bdfa26d1709cd0aae99d4ba032e84e5
Sedat Onat <sedatonat@outlook.com>
2022-10-26 04:18:56 +0300

    Update
    iOS and SwiftUI for Beginners / Part-1
    Finished

--- Tutorial_RW_BullsEye/ContentView.swift
+++ Tutorial_RW_BullsEye/ContentView.swift
@@ -15,12 +15,18 @@ struct ContentView: View {
                 .kerning(2.0)
                 .multilineTextAlignment(.center)
                 .lineSpacing(4.0)
-                .font(.footnote)
+                .font(.footnote) 
             Text("89")
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            
             HStack {
                 Text("1")
+                    .bold()
                 Slider(value: .constant(50), in: 1.0...100.0)
                 Text("100")
+                    .bold()
             }
             Button("Hit me") { }
         }
