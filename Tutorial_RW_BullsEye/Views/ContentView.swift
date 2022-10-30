c88e31f7d74f349f358c9cfd3ead1001f2e92645
Sedat Onat <sedatonat@outlook.com>
2022-10-30 05:55:30 +0300

    Update

--- Tutorial_RW_BullsEye/Views/ContentView.swift
+++ Tutorial_RW_BullsEye/Views/ContentView.swift
@@ -16,50 +16,58 @@ struct ContentView: View {
     
     var body: some View {
         
-        VStack {
-            Text("🎯🎯🎯\n Put the Bullseye as close as you can".uppercased())
-                .bold()
-                .kerning(2.0)
-                .multilineTextAlignment(.center)
-                .lineSpacing(4.0)
-                .font(.footnote)
-            Text(String(game.target)) // Text in icine Integer koyamayacagimiz icin onu String 'e cevirerek koyduk
-                .kerning(-1.0)
-                .font(.largeTitle)
-                .fontWeight(.black)
+        ZStack{
             
-            HStack {
-                Text("1")
-                    .bold()
-                Slider(value: self.$sliderValue, in: 1...100) // "in: 1...100" sonrasinda "step:1" yazarsan 1'erli basamaklarla veriyor. Ayni Class icindeyse "self." kullanimina gerek yok #learn
-                Text("100")
-                    .bold()
-            }
-            .padding()
+            Color(red: 243.0 / 255.0, green: 248.0 / 255.0, blue: 253.0 / 255.0)
+                .ignoresSafeArea()
                 
-            Button(action: {
-                print("Test")
-                self.alertIsVisible = true
-            }) {
-                Text("Hit me".uppercased())
-                    .font(.title3)
+            VStack {
+                Text("🎯🎯🎯\n Put the Bullseye as close as you can".uppercased())
                     .bold()
+                    .kerning(2.0)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                Text(String(game.target)) // Text in icine Integer koyamayacagimiz icin onu String 'e cevirerek koyduk
+                    .kerning(-1.0)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                
+                HStack {
+                    Text("1")
+                        .bold()
+                    Slider(value: self.$sliderValue, in: 1...100) // "in: 1...100" sonrasinda "step:1" yazarsan 1'erli basamaklarla veriyor. Ayni Class icindeyse "self." kullanimina gerek yok #learn
+                    Text("100")
+                        .bold()
+                }
+                .padding()
+                    
+                Button(action: {
+                    print("Test")
+                    self.alertIsVisible = true
+                }) {
+                    Text("Hit me".uppercased())
+                        .font(.title3)
+                        .bold()
+                }
+                        .padding(20.0)
+                        .background(Color.blue)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(21)
+                
+                // This whole part was copied from the Tutorial
+                .alert("Hello there!", isPresented: $alertIsVisible) {
+                    Button("Awesome!") { }
+                  } message: {
+                    let roundedValue = Int(sliderValue.rounded())
+                    Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round.")
+                  }
+                
             }
-                    .padding(20.0)
-                    .background(Color.blue)
-                    .foregroundColor(Color.white)
-                    .cornerRadius(21)
-            
-            // This whole part was copied from the Tutorial
-            .alert("Hello there!", isPresented: $alertIsVisible) {
-                Button("Awesome!") { }
-              } message: {
-                let roundedValue = Int(sliderValue.rounded())
-                Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round.")
-              }
-//        Test 7
+
             
         }
+        .ignoresSafeArea()
     }
 }
 
