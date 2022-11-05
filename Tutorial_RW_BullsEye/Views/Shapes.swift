a7559dda60528aaba4ad96ba3f6074a8aef34b20
Sedat Onat <sedatonat@outlook.com>
2022-11-05 05:38:46 +0300

    Test 2

--- Tutorial_RW_BullsEye/Views/Shapes.swift
+++ Tutorial_RW_BullsEye/Views/Shapes.swift
@@ -8,20 +8,32 @@
 import SwiftUI
 
 struct Shapes: View {
+    @State private var wideShapes = true
+    
     var body: some View {
         VStack {
-            Circle()
-                .strokeBorder(Color.blue, lineWidth: 20.0)
-                .frame(width: 200.0, height: 100.0)
+            if !wideShapes {
+                Circle()
+                    .strokeBorder(Color.blue, lineWidth: 20.0)
+                    .frame(width: 200.0, height: 100.0)
+            }
             RoundedRectangle(cornerRadius: 20.0)
                 .fill(Color.blue)
-                .frame(width: 200.0, height: 100.0)
+                .frame(width: wideShapes ? 200.0 : 100.0, height: 100.0)
             Capsule()
                 .fill(Color.blue)
-                .frame(width: 200.0, height: 100.0)
+                .frame(width: wideShapes ? 200.0 : 100.0, height: 100.0)
             Ellipse()
                 .fill(Color.blue)
-                .frame(width: 200.0, height: 100.0)
+                .frame(width: wideShapes ? 200.0 : 100.0, height: 100.0)
+            Button(
+                action:
+                    {
+                        wideShapes.toggle()
+                    })
+            {
+                Text("Animate")
+            }
         }
         .background(Color.green)
     }
